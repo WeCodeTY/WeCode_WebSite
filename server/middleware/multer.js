bc7611f5ecf348093815b6382524179d1f20ab6b
Tanush <tanushchawla16@gmail.com>
2025-04-18 19:34:41 +0530

    Update multer storage to use /tmp with unique filenames

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -2,10 +2,11 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "public/temp");
+        cb(null, "/tmp");
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname);
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
     },
 });
 
